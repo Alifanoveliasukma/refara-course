ff134efa5cfa575cad22ad1cd2d883e5275608a8
Hese11 <135138621+Hese11@users.noreply.github.com>
2024-02-19 22:53:08 +0700

    feat : soft delete category

--- database/migrations/2024_02_16_124911_create_categories_table.php
+++ database/migrations/2024_02_16_124911_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('nama_category');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
