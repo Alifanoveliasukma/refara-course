b0dafaab883570aea620f9533b5b528ab4a3d125
Hese11 <135138621+Hese11@users.noreply.github.com>
2024-02-26 23:30:25 +0700

    perf : a new registration page with a design using Tailwind CSS

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,104 +4,89 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            background-color: #f4f4f4;
+        .login-container img {
+            margin-left: 20px; 
         }
         .login-container {
-            width: 300px;
+            margin-left: 20px;
             padding: 20px;
-            background-color: #fff;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-        }
-        .login-container h2 {
-            text-align: center;
-            margin-bottom: 20px;
         }
         .form-group {
             margin-bottom: 20px;
         }
-        .form-group label {
-            display: block;
-            font-weight: bold;
-            margin-bottom: 5px;
-        }
-        .form-group input {
-            width: 100%;
-            padding: 10px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-            box-sizing: border-box;
-        }
-        .form-group input[type="submit"] {
-            background-color: #007bff;
-            color: #fff;
-            cursor: pointer;
-        }
-        .form-group input[type="submit"]:hover {
-            background-color: #0056b3;
-        }
     </style>
 </head>
-<body>
-
-<div class="login-container">
-    <h2>Register</h2>
-    @if (session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-@endif
-    <form action="{{ route('register') }}" method="POST">
-        @csrf
-        <div class="form-group">
-            <label for="name">Name</label>
-            <input type="text" name="nama" id="name" />
-            @error('nama')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
+<body class="justify-center h-screen flex items-center">
+    <script src="{{ asset('js/app.js') }}"></script>
+        <div class="w-[1124.73px] h-[611.22px] pr-[16.16px] pb-[0.22px] bg-white rounded-[43.43px] shadow justify-start items-center gap-2.5 inline-flex">
+            <div class="w-[588px] h-[611px] relative bg-orange-500 flex-col justify-start items-start flex rounded-l-[43.43px] justify-center flex items-center">
+                <div class="centered-text">
+                    <div class="text-white text-[40px] font-bold font-sans ">Hello!</div>
+                    <div class="text-white text-lg font-normal font-['Inter']">Welcome Learners</div>
+                </div>
+                @if (session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+                <form action="{{ route('register') }}" method="POST">
+                    @csrf
+                    <div >
+                        <input class="w-[352.83px] h-[55.37px] pl-[37.09px] pr-10 bg-white rounded-[30px] justify-center items-start gap-[144.78px] inline-flex mt-4" 
+                        type="text" 
+                        name="nama" 
+                        id="name" 
+                        placeholder="Masukkan nama" />
+                        @error('nama')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div >
+                        
+                        <input class="w-[352.83px] h-[55.37px] pl-[37.09px] pr-10 bg-white rounded-[30px] justify-center items-start gap-[144.78px] inline-flex mt-4" 
+                        type="text" 
+                        name="email" 
+                        id="email" 
+                        placeholder="Masukkan email" />                        
+                        @error('email')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div>
+                        <input class="w-[352.83px] h-[55.37px] pl-[37.09px] pr-10 bg-white rounded-[30px] justify-center items-start gap-[144.78px] inline-flex mt-4" 
+                        type="text" 
+                        name="password" 
+                        id="password" 
+                        placeholder="Masukkan password" />                        @error('password')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div>
+                        <input class="w-[352.83px] h-[55.37px] pl-[37.09px] pr-10 bg-white rounded-[30px] justify-center items-start gap-[144.78px] inline-flex mt-4" 
+                        type="text" 
+                        name="no_telp" 
+                        id="no_telp" 
+                        placeholder="Masukkan No HP" />                        @error('no_telp')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div>
+                        <input class="w-[352.83px] h-[55.37px] pl-[37.09px] pr-10 bg-white rounded-[30px] justify-center items-start gap-[144.78px] inline-flex mt-4" 
+                        type="text" 
+                        name="password_confirmation" 
+                        id="password_confirmation" 
+                        placeholder="Confirm Password" />                        @error('password_confirmation')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="w-[352.83px] h-[55.37px] pt-[14.31px] pb-[15.05px] bg-orange-600 rounded-[30px] justify-center items-center inline-flex mt-5">
+                        <button class="text-white text-[21.71px] font-bold font-['Inter']" type="submit">Register</button>
+                    </div>
+                </form>
+            </div>
+            <img src="{{ asset ('img/gambar-register.png') }}">
         </div>
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" />
-            @error('email')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" />
-            @error('password')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="form-group">
-            <label for="no_telp">No Hp</label>
-            <input type="no_telp" name="no_telp" id="no_telp" />
-            @error('no_telp')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="form-group">
-            <label for="password_confirmation">Confirm password</label>
-            <input type="password" name="password_confirmation" id="password_confirmation" />
-            @error('password_confirmation')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="form-group">
-            <button type="submit">Register</button>
-        </div>
-    </form>
-</div>
-
 </body>
 </html>
