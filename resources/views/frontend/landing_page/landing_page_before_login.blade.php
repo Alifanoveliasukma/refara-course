c5f44eea8080cc1b2826deb4f178afe12f63d1bd
Hese11 <135138621+Hese11@users.noreply.github.com>
2024-02-28 12:23:17 +0700

    ci : added a new landing page before login section 2 with design using Tailwind CSS.

--- resources/views/frontend/landing_page/landing_page_before_login.blade.php
+++ resources/views/frontend/landing_page/landing_page_before_login.blade.php
@@ -1,5 +1,6 @@
 @extends('frontend.test')
 @section('content')
+{{-- section 1 --}}
 <div class="w-[1366px] h-[790px] relative bg-gradient-to-r from-orange-100 to-orange-300 w-screen">
     <img class="w-[563px] h-[682px] left-[764px] top-[75px] absolute" src="{{ asset ('img/orang-landingpage.png') }}" />
     <div class="h-[272px] left-[164px] top-[222px] absolute flex-col justify-center items-start inline-flex">
@@ -17,6 +18,22 @@
             <img class="w-[166px] h-[25px]" src="{{ asset ('img/dki.png') }}" />
             <img class="w-[114px] h-16" src="{{ asset ('img/tokopedia.png') }}" />
             <img class="w-[76px] h-[31px]" src="{{ asset ('img/goto.png') }}" />
+        </div>
+    </div>
+</div>
+{{-- section 2 --}}
+<div class="w-full h-[645px] pl-[156px] pr-[157px] pt-[106px] pb-6 bg-white flex-col justify-end items-center gap-[61px] inline-flex">
+    <div class="w-[695px] h-[161px] relative flex-col justify-center items-center flex">
+        <div class="text-center text-orange-400 text-[25px] font-bold font-['Roboto'] relative h-32 w-32">Choose Us</div>
+        <div class="text-black text-[40px] font-medium font-['Roboto']">Investasikan Karir Anda Bersama Kami</div>
+        <div class="w-[664px] h-[35px] text-center text-black text-[23px] font-normal font-['Roboto']">Kursus-Kursus Terbaik yang Akan Membantu Anda Berkembang dan Sukses di Dunia Kerja</div>
+    </div>
+    <div>
+        <div class="w-[1053px] h-[293px] justify-center items-center gap-[66px] inline-flex">
+            <img class="w-[307px] h-[293px] relative  rounded-[20px]" src="{{ asset ('img/choose1.png') }} " />
+            <img class="w-[307px] h-[293px] relative  rounded-[20px]" src="{{ asset ('img/choose2.png') }}" />
+            <img class="w-[307px] h-[293px] relative  rounded-[20px]" src="{{ asset ('img/choose3.png') }}" />
+
         </div>
     </div>
 </div>
